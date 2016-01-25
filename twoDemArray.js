21f36268872b99f2dff8667f5d0237eed0fb2934
YaroslavKabanov <ya.kabanov@gmail.com>
2016-01-25 16:39:34 -0500

    added empty matrix

--- twoDemArray.js
+++ twoDemArray.js
@@ -3,11 +3,20 @@
 var mtx1 = [[1,2,3],[4,5,6],[7,8,9]]; 
 var mtx2 = [[1,1,1],[1,1,1],[1,1,1]]; 
 
+var matrix1 = [[1,2,3],[4,5,6],[7,8,9]]; 
+
+
 var matrixSum = function (matrix1, matrix2) {
 	for (var i = 0; i < matrix1.length; i++) {
 		for (var j = 0; j < matrix1.length; j++) {
-			var matrixResult = [];
-			 matrixResult.push(matrix1[i][j] + matrix2[i][j]);
+			 matrixResult[i][j] = matrix1[i][j] + matrix2[i][j];
+
+			 var matrixResult = [];
+				for (var k = 0; k < matrix1.length; k++){
+			    	matrixResult[k] = [];
+			    	for (var m = 0; m < matrix1.length; m++){
+			        	matrixResult[k][m] = 0;
+			}}
 		}
 	}
 	return matrixResult;
